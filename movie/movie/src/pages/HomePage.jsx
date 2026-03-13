cd6be5e97ca39dd0fb768c62d418c69835ce6562
sakaria27 <saka89164@gmail.com>
2026-03-13 13:04:28 +0100

    Update HomePage.jsx

--- movie/movie/src/pages/HomePage.jsx
+++ movie/movie/src/pages/HomePage.jsx
@@ -5,7 +5,20 @@ const apikey = 'aef1a365'
 const defaultsearch = 'James Bond'
 
 function HomePage() {
-
+    return(
+        <main>
+            <header>
+                <h1>Film søk</h1>
+            </header>
+            <section>
+                <h2>søk</h2>
+                <form>
+                    <label htmlFor="search">Filmtittel</label>
+                    <input type="search" id="search" />
+                </form>
+            </section>
+        </main>
+    )
 }
 
 export default HomePage
