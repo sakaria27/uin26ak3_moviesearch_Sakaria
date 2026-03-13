2b202a6d3bfbd408e823633798c750fddf702135
sakaria27 <saka89164@gmail.com>
2026-03-13 14:51:11 +0100

    Update HomePage.jsx

--- movie/movie/src/pages/HomePage.jsx
+++ movie/movie/src/pages/HomePage.jsx
@@ -2,9 +2,31 @@ import { useEffect, useState } from "react";
 import MovieList from '../componetes/MovieList'
 
 const apikey = 'aef1a365'
-const defaultsearch = 'James Bond'
+
 
 function HomePage() {
+    const [movieResults, setMovieResults] = useState([])
+    const [searchText, setSearchText ] = useState([])
+    const [laster, setLaster] = useState(true)
+    const [errorMessage, setErrorMessage] = useState("")
+
+    useEffect(() =>{
+        async function fetchMovies() {
+            setLaster(true)
+
+            const query = searchText.length >= 3 ? searchText : "James Bond"
+            const response = await fetch(
+                https://www.omdbapi.com?apikey=${apikey}=${apikey}&s=${query}&type=movie)
+            
+            const data = await response.json()
+
+            if (data.Response === 'true'){
+                if
+            }
+
+        }
+    })
+
     return(
         <main>
             <header>
@@ -19,6 +41,9 @@ function HomePage() {
             </section>
         </main>
     )
+    
+
+  
 }
 
 export default HomePage
