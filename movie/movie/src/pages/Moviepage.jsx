7f83d9fe1aa1d02d52ce3f2ed36ed2d1c359b825
sakaria27 <saka89164@gmail.com>
2026-03-15 21:16:05 +0100

    jeg klarte å legge denne koden inne i movielist

--- movie/movie/src/pages/Moviepage.jsx
+++ movie/movie/src/pages/Moviepage.jsx
@@ -1,56 +1,61 @@
 import { Link, useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
-const apikey ='aef1a365'
+const apikey = 'aef1a365'
 
 function Moviepage(){
     const location = useLocation()
-    const imbdid = location.state?.imdbid
+    const imdbID = location.state?.imdbID
 
     const [movie, setMovie] = useState(null)
     const [laster, setLaster] = useState(true)
 
-    useEffect(() =>{
+    useEffect(() => {
         async function getMovie() {
-            setLaster(true)
-
-            const response = await fetch( 
-                https://www.omdbapi.com?apikey=${apikey}&i=${imdbid})
+            const response = await fetch(`https://www.omdbapi.com?apikey=${apikey}&i=${imdbID}`)
             
             const data = await response.json()
-            if (data.Response === 'true'){
-                setMovies(data)    
-            } else{
-                setMovies(null)
+
+            if (data.Response === 'True'){
+                setMovie(data)
             }
             setLaster(false)
         }
-        if (imbdid) {
+        if (imdbID) {
             getMovie()
         }
-    },[imbdid])
+    },[imdbID])
 
     return(
         <main>
-            <nav><Link to='/'>Tilbake</Link>
+            <nav>
+                <Link to='/'>Tilbake</Link>
             </nav>
-             {laster && <p>Laster film...</p>}
-             {laster & movie && 
-             (<article>
-                <header>
-                    <h1>{movie.Title}</h1>
-                    <p>{movie.Year}</p>
-                </header>
 
-                {movie.Poster !== 'N/A' && (<img src={movie.Poster} alt={`Poster for ${movie.Title}`} />)}
-                <section>
-                    <h2>Om Filmen</h2>
-                    <p>{movie.Plot}</p>
-                </section>
-             </article>)}
+            {laster && <p>Laster film...</p>}
 
-             {!laster && !movie && <p>Fant ikke filmen</p>}
-
+            {!laster && movie && (
+                <article>
+                    <header>
+                        <h1>{movie.Title}</h1>
+                        <p>{movie.Year}</p>
+                    </header>
+                    {movie.Poster !== 'N/A' &&(
+                        <img src={movie.Poster} alt={`Poster for ${movie.Title}`} />
+                    )}
+                    <section>
+                        <h2>Om filmen</h2>
+                        <p>{movie.Plot}</p>
+                    </section>
+                    <section>
+                        <h2>Detaljer</h2>
+                        <p>Sjanger: {movie.Genre}</p>
+                        <p>Regissør: {movie.Director}</p>
+                        <p>Skuespillere: {movie.Actors}</p>
+                        <p>IMDb rating: {movie.imdbRating}</p>
+                    </section>
+                </article>
+            )}
         </main>
     )
 }
